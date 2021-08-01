edc0cc6cabf0c303ec3c47f422305c4ed9bb2ad3
clvs7 <me@clvs7.com>
2021-08-01 23:28:09 +0900

    - Update dependencies - Fix test handling

--- src/__tests__/bot-service.test.ts
+++ src/__tests__/bot-service.test.ts
@@ -6,6 +6,9 @@ import { MockConnectorService } from '../service/connector/mock-connector.servic
 describe('Testing BotService', () => {
     const mockConnector = new MockConnectorService();
     const bot = new BotService(mockConnector);
+	
+	afterAll(() => bot.finish());
+	
     it('Test version', async () => {
         const packageJson = await import(__dirname + '/../../package.json');
         expect(packageJson.version).toEqual(environment.version);
--- src/environment/environment.prod.ts
+++ src/environment/environment.prod.ts
@@ -2,7 +2,7 @@ import * as path from 'path';
 import { Environment } from '../interface/environment.interface';
 
 export const environmentProd: Environment = {
-    version: '2.1.1',
+    version: '2.1.2',
     isProduction: true,
     slack: {
         token: {
