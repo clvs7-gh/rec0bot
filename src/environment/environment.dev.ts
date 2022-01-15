bc7ad15f66cee2480ed54355fc95a0859954015e
clvs7 <me@clvs7.com>
2022-01-15 21:19:50 +0900

    Bump up version to 2.2.1

--- src/environment/environment.dev.ts
+++ src/environment/environment.dev.ts
@@ -2,7 +2,7 @@ import * as path from 'path';
 import { Environment } from '../interface/environment.interface';
 
 export const environmentDev: Environment = {
-    version: '2.2.0',
+    version: '2.2.1',
     isProduction: false,
     slack: {
         token: {
