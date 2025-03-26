9348a43d9bc1460d1fa5eae5df5ab98d21a72d29
Arisaka Mashiro <me@clvs7.com>
2025-03-26 22:11:39 +0900

    Replace ts-node with tsx

--- src/interface/bot-proxy.interface.ts
+++ src/interface/bot-proxy.interface.ts
@@ -3,7 +3,7 @@ export interface BotProxy {
     getChannelList(): Promise<any[]>;
     getChannelId(channelName: string): Promise<string>;
     getUserList(): Promise<any[]>;
-    sendTalk(channelId: string, text: string, attachmentProperty?: {[key: string]: any}[]): Promise<any>;
+    sendTalk(channelId: string, text: string, options?: {[key: string]: any}): Promise<any>;
     editTalk(channelId: string, textId: string, text: string): Promise<any>;
     sendFile(channelId: string, fileName: string, buffer: Buffer): Promise<any>;
     firePluginEvent(targetId: string, eventName: string, value?: any, fromId?: string): Promise<any>;
